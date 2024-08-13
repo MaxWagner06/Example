755a97cd2e5af8bab838a8487c5c3fd3241ad8f9
CanMuhammetAliCin <canmuhammetalicin1@gmail.com>
2024-08-14 02:07:29 +0300

    MenuUpdate1

---
+++ Example1/Assets/Scripts/MenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void LoadMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void GameQuit()
+    {
+        Application.Quit();
+    }
+}
